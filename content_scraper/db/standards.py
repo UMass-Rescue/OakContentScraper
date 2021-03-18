efa2de32765adbc2c46511dc99a1f00393b59206
Jagath Jai Kumar <jagath.jaikumar@gmail.com>
2021-03-18 13:56:36 -0400

    added standardizer

---
+++ content_scraper/db/standards.py
@@ -0,0 +1,20 @@
+from typing import List
+from pydantic import BaseModel
+from content_scraper.db.util import WrittenContentCategory
+from datetime import datetime
+
+
+class TextContent(BaseModel):
+    content: str
+    author_username: str
+    conversation_native_id: str
+    publication_date: datetime
+    publically_available: bool
+    keywords: List[str]
+    miscellanous: str
+
+
+class ScrapeResult(BaseModel):
+    platform: str
+    contents: List[TextContent]
+    content_type: WrittenContentCategory
