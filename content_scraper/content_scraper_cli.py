2804317787f3b5ed0b3af21d6d12ebd6aa7d6f0f
Jagath Jai Kumar <jagath.jaikumar@gmail.com>
2021-03-25 14:21:10 -0400

    limit

--- content_scraper/content_scraper_cli.py
+++ content_scraper/content_scraper_cli.py
@@ -14,8 +14,14 @@ if __name__ == "__main__":
         type=str,
         help="a platform to scrape from various text input sources",
     )
+    parser.add_argument(
+        "-l",
+        "--limit",
+        type=int,
+        help="number of texts per keyword",
+    )
     args = parser.parse_args()
 
-    single.batch_collect_single_platform(args.platform)
+    single.batch_collect_single_platform(args.platform, limit=args.limit)
 
     print("Single collection complete")
